43647539135788fad80d22eab589e411cdb10ff0
angeldevel200 <angeldevel200@gmail.com>
2026-04-20 10:24:13 +0600

    Add hacked message page by angeldevel200

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+<title>Hacked By angeldevel200</title>
+<meta name='author' content='Acep'/>
+<meta name='description' content='Hacked By angeldevel200'/>
+<link href='https://raw.githubusercontent.com/angeldevel200/Photos/refs/heads/main/angeldevel200.png' rel='icon' type='image/jpg'/>
+<link href='https://fonts.googleapis.com/css?family=Share' rel='stylesheet' type='text/css'/>
+  </head>
+<style>
+body {
+ font-family: "Share";
+ color: gray;
+ text-align: center;}
+</style>
+       <table width="100%" height="100%"><tr><td align="center">     
+       <img src='https://raw.githubusercontent.com/angeldevel200/Photos/refs/heads/main/angeldevel200.png' width='30%'><br><br>
+       <font size='5'>Hacked by</font><font size='5' color='red'> angeldevel200</font><br><br><font size='5'>"When You Broke My Heart We Will Broke Your
+Security"</font><br><br><br><br>
+       <font size='3' color='red'>X-GOLDEN 42<br>Community Link: https://t.me/+Aq3yDKfIngA5N2I1</font></b>
+       </table></tr></td>
+</html>
